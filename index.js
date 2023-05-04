f0d89f4e89e049b9d066436317f2d6a97906416e
developersharif1919 <developersharif1919@gmail.com>
2023-05-04 17:32:25 +0600

    Create Recipe Json File & Update Json Data

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const topChef = require('./data/top-chef.json');
+const recipes = require('./data/recipe.json');
 app.use(cors());
 
 app.get('/', (req, res) => {
@@ -18,6 +19,16 @@ app.get('/topChef/:id', (req, res) => {
     const id = req.params.id;
     const selectedChef = topChef.find(c => c.id === id);
     res.send(selectedChef)
+})
+
+app.get('/recipes', (req, res) => {
+    res.send(recipes);
+})
+
+app.get('/recipes/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedRecipes = recipes.find(r => r.id === id);
+    res.send(selectedRecipes)
 })
 
 app.listen(port, () => {
